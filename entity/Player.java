a0939db66fdbe3a3c26078c0ca6f50b449ce3a5b
Miguel Gengo <miguelgengo28@gmail.com>
2025-07-30 22:38:13 -0300

    adicionado primeiro npc do jogo

--- entity/Player.java
+++ entity/Player.java
@@ -3,13 +3,9 @@ package entity;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
-
 import main.GamePanel;
 import main.KeyHandler;
-import main.UtilityTool;
+
 
 public class Player extends Entity {
   
@@ -20,8 +16,11 @@ public class Player extends Entity {
   public final int sreenX;
   public final int screenY;
  // public int hasKey = 0;
+  int standCounter = 0;
 
   public Player(GamePanel gp, KeyHandler keyH){
+    super(gp);
+
     this.gp = gp;
     this.keyH = keyH;
 
@@ -48,28 +47,17 @@ public class Player extends Entity {
   }
   public void getPlayerImage(){
 
-    up1 = setup("boy_up_1");
-    up2 = setup("boy_up_2");
-    down1 = setup("boy_down_1");
-    down2 = setup("boy_down_2");
-    left1 = setup("boy_left_1");
-    left2 = setup("boy_left_2");
-    right1 = setup("boy_right_1");
-    right2 = setup("boy_right_2");
+    up1 = setup("/res/player/boy_up_1");
+    up2 = setup("/res/player/boy_up_2");
+    down1 = setup("/res/player/boy_down_1");
+    down2 = setup("/res/player/boy_down_2");
+    left1 = setup("/res/player/boy_left_1");
+    left2 = setup("/res/player/boy_left_2");
+    right1 = setup("/res/player/boy_right_1");
+    right2 = setup("/res/player/boy_right_2");
 
   }
-  public BufferedImage setup(String imageName){
-    UtilityTool uTool = new UtilityTool();
-    BufferedImage image = null;
-    try{
-      image = ImageIO.read(getClass().getResourceAsStream("/res/player/" + imageName + ".png"));
-      image = uTool.scaleImage(image, gp.tileSize, gp.tileSize);
-
-    } catch(IOException e){
-      e.printStackTrace();
-    }
-    return image;
-  }
+  
   public void update(){
 
     if(keyH.upPressed == true || keyH.downPressed == true || 
@@ -98,6 +86,10 @@ public class Player extends Entity {
               //CHECK OBJECT COLLISION
              int objIndex = gp.cChecker.checkObject(this,true);
              pickUpObject(objIndex);
+
+             //CHECK MPC COLLISION
+             int ncpIndex = gp.cChecker.checkEntity(this, gp.npc);
+             interactNPC(ncpIndex);
 
               //IF COLLISION IS FALSE, PLAYER CAN MOVE
               if(collisionOn == false){
@@ -133,6 +125,11 @@ public class Player extends Entity {
   public void pickUpObject(int i){
     if(i != 999){
    
+    }
+  }
+  public void interactNPC(int i){
+    if(i != 999){
+      System.out.println("you are hiting an npc");
     }
   }
   public void draw(Graphics2D g2){
--- main/CollisionChecker.java
+++ main/CollisionChecker.java
@@ -129,4 +129,116 @@ public class CollisionChecker {
     }
     return index;
 }
+//NPC OR MONSTER 
+public int checkEntity(Entity entity, Entity[] target){
+  int index = 999;                          // 999 = não colidiu
+
+  for (int i = 0; i < target.length; i++) {
+      if (target[i] != null) {
+
+          /* ---------- prepara áreas sólidas absolutas ---------- */
+          // player (entity)
+          entity.solidArea.x = entity.worldX + entity.solidAreaDefaultX;
+          entity.solidArea.y = entity.worldY + entity.solidAreaDefaultY;
+
+          // objeto
+          target[i].solidArea.x = target[i].worldX + target[i].solidAreaDefaultX;
+          target[i].solidArea.y = target[i].worldY + target[i].solidAreaDefaultY;
+
+          /* ---------- simula o próximo passo do player ---------- */
+          switch (entity.direction) {
+              case "up":
+               entity.solidArea.y -= entity.speed;
+               if(entity.solidArea.intersects(target[i].solidArea)){
+              
+                  entity.collisionOn = true;
+                  index = i; 
+               }
+               break;
+              case "down":
+               entity.solidArea.y += entity.speed;
+               if(entity.solidArea.intersects(target[i].solidArea)){
+               
+                  entity.collisionOn = true;
+                  index = i;     
+               }
+               break;
+              case "left":
+               entity.solidArea.x -= entity.speed;
+               if(entity.solidArea.intersects(target[i].solidArea)){
+             
+                  entity.collisionOn = true;
+                  index = i;
+                
+               }
+               break;
+              case "right":
+               entity.solidArea.x += entity.speed;
+               if(entity.solidArea.intersects(target[i].solidArea)){
+                
+                  entity.collisionOn = true;
+                  index = i;
+               }
+               break;
+          }
+          /* ---------- restaura áreas sólidas ---------- */
+          entity.solidArea.x  = entity.solidAreaDefaultX;
+          entity.solidArea.y  = entity.solidAreaDefaultY;
+          target[i].solidArea.x = target[i].solidAreaDefaultX;
+          target[i].solidArea.y = target[i].solidAreaDefaultY;
+      }
+  }
+  return index;
+}
+public void checkPlayer(Entity entity){
+     /* ---------- prepara áreas sólidas absolutas ---------- */
+          // player (entity)
+          entity.solidArea.x = entity.worldX + entity.solidAreaDefaultX;
+          entity.solidArea.y = entity.worldY + entity.solidAreaDefaultY;
+
+          // objeto
+          gp.player.solidArea.x = gp.player.worldX + gp.player.solidAreaDefaultX;
+          gp.player.solidArea.y = gp.player.worldY + gp.player.solidAreaDefaultY;
+
+          /* ---------- simula o próximo passo do player ---------- */
+          switch (entity.direction) {
+              case "up":
+               entity.solidArea.y -= entity.speed;
+               if(entity.solidArea.intersects(gp.player.solidArea)){
+              
+                  entity.collisionOn = true; 
+               }
+               break;
+              case "down":
+               entity.solidArea.y += entity.speed;
+               if(entity.solidArea.intersects(gp.player.solidArea)){
+               
+                  entity.collisionOn = true;
+                      
+               }
+               break;
+              case "left":
+               entity.solidArea.x -= entity.speed;
+               if(entity.solidArea.intersects(gp.player.solidArea)){
+             
+                  entity.collisionOn = true;
+                 
+                
+               }
+               break;
+              case "right":
+               entity.solidArea.x += entity.speed;
+               if(entity.solidArea.intersects(gp.player.solidArea)){
+                
+                  entity.collisionOn = true;
+                
+               }
+               break;
+          }
+          /* ---------- restaura áreas sólidas ---------- */
+          entity.solidArea.x  = entity.solidAreaDefaultX;
+          entity.solidArea.y  = entity.solidAreaDefaultY;
+          gp.player.solidArea.x = gp.player.solidAreaDefaultX;
+          gp.player.solidArea.y = gp.player.solidAreaDefaultY;
+}
 }
